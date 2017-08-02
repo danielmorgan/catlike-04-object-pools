880fb05d49e73ee5f297c7f4fee58daf09cb3c71
Daniel Morgan <me@danielmorgan.co.uk>
2017-08-02 22:55:35 +0100

    Set up scene switching

---
+++ Assets/SceneSwitcher.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+    
+    public void SwitchScene () {
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        int nextScene = currentScene.buildIndex == 0 ? 1 : 0;
+
+        SceneManager.LoadScene(nextScene);
+    }
+}
