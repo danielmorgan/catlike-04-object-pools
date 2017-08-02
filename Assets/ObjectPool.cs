73dc7db6f155a461c1225169e260efc6721da361
Daniel Morgan <me@danielmorgan.co.uk>
2017-08-02 22:02:02 +0100

    Make use of object pools

--- Assets/ObjectPool.cs
+++ Assets/ObjectPool.cs
@@ -1,18 +1,32 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class ObjectPool : MonoBehaviour {
 
     PooledObject prefab;
 
+    List<PooledObject> availableObjects = new List<PooledObject>();
+
     public PooledObject GetObject () {
-        PooledObject obj = Instantiate<PooledObject>(prefab);
-        obj.transform.SetParent(transform, false);
-        obj.Pool = this;
+        PooledObject obj;
+        int lastAvailableIndex = availableObjects.Count - 1;
+
+        if (lastAvailableIndex >= 0) {
+            obj = availableObjects[lastAvailableIndex];
+            availableObjects.RemoveAt(lastAvailableIndex);
+            obj.gameObject.SetActive(true);
+        } else {
+            obj = Instantiate<PooledObject>(prefab);
+            obj.transform.SetParent(transform, false);
+            obj.Pool = this;
+        }
+
         return obj;
     }
 
-    public void AddObject (PooledObject o) {
-        Object.Destroy(o.gameObject);
+    public void AddObject (PooledObject obj) {
+        obj.gameObject.SetActive(false);
+        availableObjects.Add(obj);
     }
 
     public static ObjectPool GetPool (PooledObject prefab) {
---
+++ README.md
@@ -0,0 +1,1 @@
+![](screenshot01.png?raw=true)
